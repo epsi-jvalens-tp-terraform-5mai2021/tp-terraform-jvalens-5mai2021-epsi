1bd8e4d7ec8d219f2aa25b61d75c6618f939151c
jeroval <78859999+jeroval@users.noreply.github.com>
2021-05-05 11:01:38 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,27 @@
+variable "name" {
+    
+    type = string
+    default = "mon_application"
+    description = "application"
+}
 
+variable "username" {
+    
+    type = string
+    default = "usernemae"
+    description = "insert_username"
+}
+
+variable "permission_pull" {
+    
+    type = string
+    default = "pull"
+    description = "permission de pull"
+}
+
+variable "permission_push" {
+    
+    type = string
+    default = "pull"
+    description = "permission de push"
+}
